5a6a0dd8dc475fb55818306ecc5d714d6f821c3c
jannikvlk <k-c-i-n@gmx.de>
2021-12-13 17:45:05 +0100

    Snacke moves and crashes

--- snake.py
+++ snake.py
@@ -4,6 +4,13 @@ from typing import Optional, List, Tuple
 import time
 
 TILE_SIZE = 20
+SNAKE_HEAD = (50, 205, 50)
+SNAKE_BODY = (40, 195, 40)
+
+RIGHT = (1, 0)
+LEFT = (-1, 0)
+UP = (0, -1)
+DOWN = (0, 1)
 
 
 # TODO: Spielklassen
@@ -13,11 +20,13 @@ class Item:
         self._x = x
         self._y = y
 
+    # returns whether both given coordinates match with the objects coordinates
     def occupies(self, x: int, y: int) -> bool:
         if x == self._x and y == self._y:
             return True
         return False
 
+    # returns a string that represents this object of item with the two attributes x and y
     def __repr__(self):
         return f'x = {self._x}, y = {self._y}'
 
@@ -34,6 +43,76 @@ class Brick(Item):
                           TILE_SIZE,
                           TILE_SIZE]
                          )
+
+
+class Snake:
+    def __init__(self, x: int, y: int) -> None:
+        self._occupies = [(x, y)]
+        self._direction = tuple[int, int]
+        self._grow = 0
+
+        self.set_direction(RIGHT)
+        self.grow(3)
+
+    def get_head(self) -> Tuple[int, int]:
+        return self._occupies[0]
+
+    def occupies(self, x: int, y: int) -> bool:
+        print('check occupied')
+        for coordinate in self._occupies:
+            print(coordinate, x, y)
+
+            if x == coordinate[0] and y == coordinate[1]:
+                return True
+        return False
+
+    def draw(self, surface: pygame.Surface) -> None:
+
+        for ind, part in enumerate(self._occupies):
+            if ind != 0:
+                color = SNAKE_BODY
+            else:
+                color = SNAKE_HEAD
+
+            pygame.draw.rect(surface,
+                             color,
+                             [TILE_SIZE * part[0],
+                              TILE_SIZE * part[1],
+                              TILE_SIZE,
+                              TILE_SIZE]
+                             )
+
+    def set_direction(self, direction: tuple[int, int]) -> None:
+        if direction == RIGHT or direction == LEFT or direction == UP or direction == DOWN:
+            self._direction = direction
+        else:
+            raise Exception('Wrong direction')
+
+    # The snake can only grow 1 field per tick -> grow can only be 1 or 0 to prevent errors
+    def grow(self, g: int) -> None:
+        if g > 0:
+            self._grow = 1
+
+        # TODO weitere Body teile hinzufügen? Nein?
+
+    def step(self, forbidden: list[Item]) -> bool:
+        new_field = (self._occupies[0][0] + self._direction[0], self._occupies[0][1] + self._direction[1])
+
+        for forbidden_field in forbidden:
+            print(f'{new_field} ?= {forbidden_field}')
+            if forbidden_field.occupies(new_field[0], new_field[1]):
+                return False
+
+        if self.occupies(new_field[0], new_field[1]):
+            return False
+
+        self._occupies.insert(0, new_field)
+        if self._grow == 0:
+            self._occupies.pop()
+        else:
+            self._grow = 0
+
+        return True
 
 
 def main():
@@ -59,6 +138,7 @@ def main():
         if x == 0 or x == width - 1 or y == 0 or y == height - 1
     ]
 
+    snake = Snake(width // 2, height // 2)
     # TODO zusätzliche Steine im innern erstellen
     # wall.append(Brick(7,7))
 
@@ -66,6 +146,7 @@ def main():
     while running:
         screen.fill((20, 20, 20))
 
+        # draws every brick in wall on 'screen'
         for brick in wall:
             brick.draw(screen)
 
@@ -76,24 +157,20 @@ def main():
                 break
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    # TODO: Richtung aendern: nach links
-                    pass
+                    snake.set_direction(LEFT)
                 elif event.key == pygame.K_RIGHT:
-                    # TODO: Richtung aendern: nach rechts
-                    pass
+                    snake.set_direction(RIGHT)
                 elif event.key == pygame.K_UP:
-                    # TODO: Richtung aendern: nach oben
-                    pass
+                    snake.set_direction(UP)
                 elif event.key == pygame.K_DOWN:
-                    # TODO: Richtung aendern: nach unten
-                    pass
+                    snake.set_direction(DOWN)
 
         if not running:
             break
 
-        # TODO: Schlange bewegen
-
-        # TODO: Schlange zeichnen
+        running = snake.step(wall)
+        print(running)
+        snake.draw(screen)
 
         # TODO: Ueberpruefen, ob die Kirsche erreicht wurde, falls ja, wachsen und Kirsche bewegen.
 
